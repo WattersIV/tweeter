3bd9b09bf31b42187cc93901b4b7a90853a47c70
Bill Watters <billywatters13@gmail.com>
2020-10-06 20:15:38 +0000

    Added color change to red when char count < 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  $("#tweet-text").on("input",function() {
-    let counter = 140 - $(this).val().length - 1; 
-    $(this).closest("form").find('#counter').text(counter); 
-
+  $("#tweet-text").on('input', function() {
+    let counter = 140 - $(this).val().length; 
+    const charCount = $(this).closest('form').find('#counter').text(counter); 
+    if (counter < 0) {
+      charCount.css('color', 'red')
+    } else {
+      charCount.css('color', 'white')
+    }
   })
 }); 
 
-// .css("color", "red")
